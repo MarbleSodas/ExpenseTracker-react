4bf96ec4616052cafc41c533dd92833322ae1a9b
Eugene Park <eugenepark404@gmail.com>
2022-04-02 20:29:24 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Header} from './Components/Header';
+import { Balance } from './Components/Balance';
+import { IncomeExpenses } from './Components/IncomeExpenses';
+import { TransactionList } from './Components/TransactionList';
+import { AddTransaction } from './Components/AddTransaction';
+
+import { GlobalProvider } from './Context/GlobalState';
+
+import './App.css';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
