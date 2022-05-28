686d7642a291252517e630f39feceb8918d0c837
Eugene Park <eugenepark404@gmail.com>
2022-05-27 19:00:08 -0700

    Web package

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react';
 import { GlobalContext } from '../Context/GlobalState';
+import {numberWithCommas} from '../utils/format';
 
 export const Transaction = ({ transaction }) => {
   const {deleteTransaction} = useContext(GlobalContext);
@@ -7,8 +8,8 @@ export const Transaction = ({ transaction }) => {
   const sign = transaction.amount < 0 ? '-' : '+';
   return (
     <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
-        {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button 
-        onClick={() => deleteTransaction(transaction.id)}className="delete-btn">x</button>
+        {transaction.text} <span>{sign}${numberWithCommas(Math.abs(transaction.amount))}</span><button 
+        onClick={() => deleteTransaction(transaction._id)}className="delete-btn">x</button>
     </li>
   )
 }
--- src/Components/TransactionList.js
+++ src/Components/TransactionList.js
@@ -1,15 +1,20 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Transaction } from './Transaction';
 import { GlobalContext } from '../Context/GlobalState';
 
 export const TransactionList = () => {
-  const { transactions } = useContext(GlobalContext);
+  const { transactions, getTransactions } = useContext(GlobalContext);
+
+  useEffect(() => {
+    getTransactions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
-    <h3>History</h3>
+      <h3>History</h3>
       <ul className="list">
-        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+        {transactions.map(transaction => (<Transaction key={transaction._id} transaction={transaction} />))}
       </ul>
     </>
   )
